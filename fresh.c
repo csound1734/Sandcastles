65b891cfa526d1c923287942fdcebf00101122cb
csound1734 <63118101+csound1734@users.noreply.github.com>
2021-03-09 08:15:05 -0500

    cleanup

--- fresh.c
+++ fresh.c
@@ -8,22 +8,6 @@
 	 size_t size;
  } ;
 
- size_t writedata(void *contents, size_t size, size_t nmemb, void *userp) {
-	 size_t realsize = size*nmemb;
-	 struct MemoryString *mem = (struct MemoryString *)userp;
-	 char *ptr = realloc(mem->memory, mem->size + realsize + 1);
-	 if(ptr == NULL) {
-		 /* out of memory! */
-		 printf("not enough memory");
-		 return 0;
-	 }
-	 mem->memory = ptr;
-	 memcpy(&(mem->memory[mem->size]), contents, realsize);
-	 mem->size += realsize;
-	 mem->memory[mem->size] = 0;
-	 return realsize;
- }
-
 size_t header_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
 	size_t numbytes = size * nmemb;
 	struct MemoryString *userp = (struct MemoryString *)userdata;
@@ -61,8 +45,6 @@ int main(int argc, char **argv) {
 		}
 	}
 	 printf("\nRefresh Token Detected: %s\n\n", refreshToken);
-	 chunk.memory = malloc(1);
-	 chunk.size = 0;
 	 curl = curl_easy_init();
 	 char *myurl = "https://freesound.org/apiv2/oauth2/access_token/";
 	 char *mypost = "client_id=1k4d2Azct3D650WgIFbh&client_secret=bgTSKIoy9dgSWhJhJSSgyHhyvBBJuyNewNkjjaBx&grant_type=refresh_token&refresh_token=";
@@ -71,8 +53,6 @@ int main(int argc, char **argv) {
 	 if(curl) {
 		 CURLcode res;
 		 curl_easy_setopt(curl, CURLOPT_URL, myurl);
-		 //curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writedata);
-		 //curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 		 curl_easy_setopt(curl, CURLOPT_POSTFIELDS, result);
 		 curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_callback);
 		 curl_easy_setopt(curl, CURLOPT_HEADERDATA, &headchk);
